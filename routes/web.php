e64517edc7f36abb0bde5e7ad255db20e6ff185d
kawamoto <legoburokku1@gmail.com>
2021-10-24 11:38:29 +0000

    create_order

--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,5 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
      Route::resource('categories', 'Dashboard\CategoryController')->middleware('auth:admins');
      Route::resource('products', 'Dashboard\ProductController')->middleware('auth:admins');
      Route::resource('users', 'Dashboard\UserController')->middleware('auth:admins');
+     Route::get('orders', 'Dashboard\OrderController@index')->middleware('auth:admins');
  });
